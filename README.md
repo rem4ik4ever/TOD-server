f21b43549ab108af02b3e7988a78a55c399ee701
Rem Kim <rem4ik4ever@gmail.com>
2021-06-19 09:28:59 -0400

    initial commit
    Signed-off-by: Rem Kim <rem4ik4ever@gmail.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# ExpressJS + TypeScript nodes.js server
+
+This is a pre-configured node.js server build with ExpressJS and TypeScript
+
+
+Tools:
+- husky
+- eslint
+
+
+
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+import { opsBasePath, opsMiddleware } from './middlewares/ops';
+import {
+  getStaticPath,
+  getVirtualRoot,
+  serveStaticFiles
+} from './staticConfig';
+
+// initialize configuration
+dotenv.config();
+
+const port: string = (process.env.SERVER_PORT != null) ? process.env.SERVER_PORT : '8080';
+const app = express();
+
+app.use(opsBasePath, opsMiddleware);
+
+if (serveStaticFiles()) {
+  app.use(getVirtualRoot(), express.static(getStaticPath()));
+}
+
+// define a route handler for the default home page
+app.get('/', (req: Request, res: Response) => {
+  res.send(`Hello world from ${req.hostname}`);
+});
+
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
